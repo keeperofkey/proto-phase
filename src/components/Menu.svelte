78e413ee9324d14571416e3e8b0c8a7d4840d768
keeperofkey <doddtastic@gmail.com>
2023-05-15 14:32:21 -0400

    working-on-menu

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,6 +1,19 @@
-<script>
-    // your script goes here
+<script lang='ts'>
+  import { Canvas, T } from '@threlte/core'
 </script>
-<div>
-    
+<div class="cube">
+    <Canvas>
+       <!-- <T.OrthographicCamera makeDefault position={[10 ,10, 10]} up={[0, 0, 1]} lookAt={[1 , 2, 1]} />-->
+
+        <T.Mesh position.y={1}>
+            <T.BoxGeometry args={[1, 2, 1]} />
+            <T.MeshBasicMaterial color="hotpink" />
+        </T.Mesh>
+    </Canvas>
 </div>
+<style>
+    .cube {
+        height: 100%;
+        width: 100%;
+    }
+</style>
